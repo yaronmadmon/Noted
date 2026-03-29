af13735258e51c33c9ef566101c0f2954ead3fd0
Yaron Madmon <yaronmadmon@gmail.com>
2026-03-29 19:50:14 -0400

    fix: mark pdf-parse as serverExternalPackage to prevent build-time init

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ const createNextIntlPlugin = require('next-intl/plugin')
 const withNextIntl = createNextIntlPlugin('./i18n/request.ts')
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  serverExternalPackages: ['pdf-parse'],
+}
 
 module.exports = withNextIntl(nextConfig)
