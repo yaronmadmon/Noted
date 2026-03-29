16651175bf2869dc3db0418ca17dd957b5a7bee4
Yaron Madmon <yaronmadmon@gmail.com>
2026-03-29 12:42:19 -0400

    fix: exclude auth/callback from intl middleware

--- middleware.ts
+++ middleware.ts
@@ -44,5 +44,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next|.*\\..*).*)'],
+  matcher: ['/((?!api|_next|auth/callback|.*\\..*).*)'],
 }
